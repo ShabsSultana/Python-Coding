c75a27c6d62d409f6f6ac8521b93afe6f3d1b163
ShabsSultana <shabnur_sultana@hotmail.com>
2020-10-01 16:38:16 +0100

    Week 1: Python printing

---
+++ Week1-Printing.py
@@ -0,0 +1,8 @@
+# printing a string
+print("Hi")
+# printing an int
+print(3 + 3)
+# asking for user input and saving it a a variable (name)
+name = input("What is your name? ")
+# prints greeting and user's name
+print("Hi " + name + ", nice to meet you!")
